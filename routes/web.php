0c5a87b131c521e950dab5b234c93f554648e084
Andreas Rizki Rizaldi <arizkirizaldi@mahasiswa.unikom.ac.id>
2021-09-27 14:48:19 +0700

    Protect Route

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [\App\Http\Controllers\Front::class,'index'])->name('halaman_utama');
-
 Auth::routes(['register' => false]);
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/dashboard/daftardonatur',[\App\Http\Controllers\HomeController::class,'daftarDonatur'])->name('dashboard.daftardonatur');
-
+Route::get('/', [\App\Http\Controllers\Front::class,'index'])->name('halaman_utama');
 Route::resource('donasi',\App\Http\Controllers\Donasi::class);
 Route::get("/donasi/validate/{id}",[\App\Http\Controllers\Donasi::class,'validateDonation']);
-
-Route::resource('rekening',\App\Http\Controllers\Rekening::class);
-Route::resource('gambar',\App\Http\Controllers\Gambar::class);
-
-Route::get('/validate',[\App\Http\Controllers\Donasi::class,'be_validate']);
 Route::get('/galery',[\App\Http\Controllers\Front::class,'galery']);
 Route::get('/daftardonatur',[\App\Http\Controllers\Front::class,'daftardonatur']);
-Route::get('/logout', [\App\Http\Controllers\Auth\LoginController::class,'logout']);
+
+//Dashboard
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/dashboard/daftardonatur',[\App\Http\Controllers\HomeController::class,'daftarDonatur'])->name('dashboard.daftardonatur');
+    Route::get('/validate',[\App\Http\Controllers\Donasi::class,'be_validate']);
+    Route::resource('rekening',\App\Http\Controllers\Rekening::class);
+    Route::resource('gambar',\App\Http\Controllers\Gambar::class);
+    Route::get('/logout', [\App\Http\Controllers\Auth\LoginController::class,'logout']);
+});
